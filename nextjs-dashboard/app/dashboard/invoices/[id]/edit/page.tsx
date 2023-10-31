86caafec323485dca53c1e26d2399e67f7e0aaea
Raja Parsaulian <raja.parsaulian@gmail.com>
2023-10-31 12:28:02 +0000

    Fix data type InvoiceForm

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { InvoiceForm } from '@/app/lib/definitions';
  
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -21,7 +22,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers} />
+      <Form invoice={invoice as InvoiceForm} customers={customers} />
     </main>
   );
 }
